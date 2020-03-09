4b8a9e38f6a1dad84f566948c38f1f14de9d6bac
Evan Haggard <evanhaggard123@gmail.com>
2020-03-09 16:17:50 -0400

    second commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan  = new Scanner(System.in);
+        System.out.println("Type Age Below: ");
+        String inputA = scan.nextLine();
+
+        System.out.println("You Are " + inputA);
+
+    }
+}
